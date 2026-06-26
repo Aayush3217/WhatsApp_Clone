6770d0b46d3829c2627bbbaf42b5e7b6a3e273fa
Aayush pal <Aayushpal3217@gmail.com>
2026-06-26 21:26:28 +0530

    Fix Bugs

--- src/pages/chatSection/ChatWindow.jsx
+++ src/pages/chatSection/ChatWindow.jsx
@@ -191,33 +191,11 @@ const ChatWindow = ({ selectedContact, setSelectedContact, isMobile }) => {
           <FaArrowLeft className='h-6 w-6' />
         </button>
         <img src={selectedContact?.profilePicture} alt={selectedContact?.username} className='w-10 h-10 rounded-full' />
-        {/* <img
-          src={
-            selectedContact?.profilePicture ||
-            `https://ui-avatars.com/api/?name=${encodeURIComponent(
-              selectedContact?.username ||
-              selectedContact?.phoneNumber ||
-              selectedContact?.email ||
-              "User"
-            )}`
-          }
-          alt={
-            selectedContact?.username ||
-            selectedContact?.phoneNumber ||
-            selectedContact?.email
-          }
-          className="w-10 h-10 rounded-full"
-        /> */}
 
         <div className='ml-3 flex-grow'>
           <h2 className='font-semibold text-start'>
             {selectedContact?.username}
           </h2>
-          {/* <h2 className="font-semibold text-start">
-            {selectedContact?.username ||
-              selectedContact?.phoneNumber ||
-              selectedContact?.email}
-          </h2> */}
 
           {isTyping ? (
             <div>Typing...</div>
