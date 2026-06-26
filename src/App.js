103ba53d6b93e70507f904e62316192d56cd2e58
Aayush pal <Aayushpal3217@gmail.com>
2026-06-26 22:09:30 +0530

    Fix Bugs

--- src/App.js
+++ src/App.js
@@ -1,52 +1,120 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/user-login/Login';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import {ProtectedRoute, PublicRoute} from './Protected';
-import HomePage from './components/HomePage';
-import UserDetails from './components/UserDetails';
-import Status from './pages/StatusSection/Status'
-import Setting from './pages/SettingSection/Setting'
-import UserStore from './store/useUserStore';
-import { disconnectSocket, initializeSocket } from './services/chat.service';
-import { useChatStore } from './store/chatStore';
+// import React, { useEffect } from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Login from './pages/user-login/Login';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import {ProtectedRoute, PublicRoute} from './Protected';
+// import HomePage from './components/HomePage';
+// import UserDetails from './components/UserDetails';
+// import Status from './pages/StatusSection/Status'
+// import Setting from './pages/SettingSection/Setting'
+// import UserStore from './store/useUserStore';
+// import { disconnectSocket, initializeSocket } from './services/chat.service';
+// import { useChatStore } from './store/chatStore';
 
+
+// function App() {
+
+//   const {user} = UserStore();  
+//   const {setCurrentUser, initsocketListners, cleanup} = useChatStore();
+  
+//   useEffect(() => {
+//     if(user?._id){
+//       const socket = initializeSocket();
+
+//       if(socket){
+//         setCurrentUser(user);
+
+//         initsocketListners();
+//       }
+//     }
+//     return () => {
+//       cleanup();
+//       disconnectSocket();
+//     }
+//   }, [user, setCurrentUser, initializeSocket, cleanup])
+
+//   return (
+//     <>
+//       <ToastContainer position='top-right' autoClose={3000} />
+//       <Router>
+//         <Routes>
+//           <Route element={<PublicRoute/>}>
+//             <Route path='/user-login' element={<Login />} />
+//           </Route>
+//           <Route element={<ProtectedRoute />}>
+//             <Route path='/' element={<HomePage />}/>
+//             <Route path='/user-profile' element={<UserDetails />}/>
+//             <Route path='/status' element={<Status />}/>
+//             <Route path='/setting' element={<Setting />}/>
+//           </Route>
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/user-login/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ProtectedRoute, PublicRoute } from "./Protected";
+
+import HomePage from "./components/HomePage";
+import UserDetails from "./components/UserDetails";
+import Status from "./pages/StatusSection/Status";
+import Setting from "./pages/SettingSection/Setting";
+
+import UserStore from "./store/useUserStore";
+import { disconnectSocket, initializeSocket } from "./services/chat.service";
+import { useChatStore } from "./store/chatStore";
 
 function App() {
+  const { user } = UserStore();
+  const { setCurrentUser, initsocketListners, cleanup } = useChatStore();
 
-  const {user} = UserStore();  
-  const {setCurrentUser, initsocketListners, cleanup} = useChatStore();
-  
   useEffect(() => {
-    if(user?._id){
+    if (user?._id) {
       const socket = initializeSocket();
 
-      if(socket){
+      if (socket) {
         setCurrentUser(user);
-
         initsocketListners();
       }
     }
+
     return () => {
       cleanup();
       disconnectSocket();
-    }
-  }, [user, setCurrentUser, initializeSocket, cleanup])
+    };
+  }, [user]);
 
   return (
     <>
-      <ToastContainer position='top-right' autoClose={3000} />
+      <ToastContainer position="top-right" autoClose={3000} />
+
       <Router>
         <Routes>
-          <Route element={<PublicRoute/>}>
-            <Route path='/user-login' element={<Login />} />
+          <Route element={<PublicRoute />}>
+            <Route path="/user-login" element={<Login />} />
           </Route>
+
           <Route element={<ProtectedRoute />}>
-            <Route path='/' element={<HomePage />}/>
-            <Route path='/user-profile' element={<UserDetails />}/>
-            <Route path='/status' element={<Status />}/>
-            <Route path='/setting' element={<Setting />}/>
+            <Route path="/" element={<HomePage />} />
+
+            {/* Profile Page */}
+            <Route path="/user-profile" element={<UserDetails />} />
+
+            {/* Same page for first-time login */}
+            <Route path="/update-profile" element={<UserDetails />} />
+
+            <Route path="/status" element={<Status />} />
+            <Route path="/setting" element={<Setting />} />
           </Route>
         </Routes>
       </Router>
